31fd334202ace26dd3ed9e930942c0f52435501f
yulin0625 <yulin90625@gmail.com>
2024-04-18 21:35:33 +0800

    Build grocery_list UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/screens/grocery.dart';
+import 'package:shopping_list/widget/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: const Grocery(),
+      home: const GroceryList(),
     );
   }
 }
--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 
-class GroceryList extends StatefulWidget {
+class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
-  @override
-  State<GroceryList> createState() {
-    return _GroceryListState();
-  }
-}
 
-class _GroceryListState extends State<GroceryList> {
-  final groceryItem = groceryItems;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        for (final item in groceryItem)
-          ListTile(
-            leading: Container(
-              height: 30,
-              width: 30,
-              color: item.category.color,
-            ),
-            title: Text(item.name),
-            trailing: Text(item.quantity.toString()),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
           ),
-      ],
+          title: Text(groceryItems[index].name),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
+      ),
     );
   }
 }
