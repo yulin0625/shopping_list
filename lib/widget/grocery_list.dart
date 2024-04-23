8068b5306f50e103f69faea491ce0325424af8c5
yulin0625 <yulin90625@gmail.com>
2024-04-23 17:33:06 +0800

    Error response handling

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -28,6 +29,13 @@ class _GroceryListState extends State<GroceryList> {
         'flutter-shoppinglist-app-bdf3d-default-rtdb.firebaseio.com',
         'shopping-list.json');
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later.';
+      });
+    }
+
     print(response.body);
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadItems = [];
@@ -100,6 +108,10 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
